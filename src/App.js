2222f5e93e33357d9a5bc7a8c673045a4a3f5c77
cdfishe1 <75449671+cdfishe1@users.noreply.github.com>
2021-06-16 10:28:28 -0500

    remove unnecessary css. Install react-strap-table for directory

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import React, { useState, useEffect } from "react";
+import React from 'react';
+import Directory from './components/Table/index.js';
 
 
 function App() {
-  
+  return (
+    <div className="App">
+      <div className="container"></div>
+      <Table />
+    </div>
+  );
 }
 
 export default App;
